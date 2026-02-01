1a40b85469a2301453b8c640f54b01256707f6c7
Nguyễn Minh Tuấn <39901437+tuanbk654123@users.noreply.github.com>
2026-02-01 19:11:34 +0700

    Hardcode API_BASE_URL to empty string

--- Qu-n-l-kh-ch-h-ng-qu-n-l-t-i-ch-nh/src/config/api.js
+++ Qu-n-l-kh-ch-h-ng-qu-n-l-t-i-ch-nh/src/config/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || '';
+const API_BASE_URL = ''; // Hardcode to empty string to prevent double /api/api issue
+// const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || '';
 
 axios.defaults.baseURL = API_BASE_URL;
 
