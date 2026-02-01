1ad1cc0adbb6d377b3bfea59a6a02e32486dbec1
Nguyễn Minh Tuấn <39901437+tuanbk654123@users.noreply.github.com>
2026-02-01 17:26:34 +0700

    Fix API URL and Nginx proxy

--- Qu-n-l-kh-ch-h-ng-qu-n-l-t-i-ch-nh/src/config/api.js
+++ Qu-n-l-kh-ch-h-ng-qu-n-l-t-i-ch-nh/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:58457';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || '/api';
 
 axios.defaults.baseURL = API_BASE_URL;
 
